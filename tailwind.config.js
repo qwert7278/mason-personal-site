e2d65f531003221052c28cd17a246da52236880b
Mason <61769533+qwert7278@users.noreply.github.com>
2025-04-14 17:01:42 +0800

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,9 +2,12 @@ module.exports = {
   content: [
     "./pages/**/*.{js,ts,jsx,tsx}",
     "./components/**/*.{js,ts,jsx,tsx}",
+    "./app/**/*.{js,ts,jsx,tsx}",
+    "./lib/**/*.{js,ts,jsx,tsx}",
+    "./styles/**/*.{css,js}"
   ],
   theme: {
     extend: {},
   },
   plugins: [],
-};
+}
